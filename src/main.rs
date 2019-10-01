8e9dbe0a35710b93f8c9003cafddf3ab37a52a0f
sdfnz <30536578+sdfnz@users.noreply.github.com>
2019-10-01 17:26:56 -0500

    Expanded create function and laid groundwork for finishing its functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::env;
 use std::path::Path;
 use std::fs;
-use std::io::{ErrorKind};
-
-#[macro_use]
-extern crate json;
+use std::io::Read;
 
 
 fn main() {
@@ -13,7 +10,7 @@ fn main() {
     if args.len() <= 1 {
         println!("Please provide a subcommand and arguments. Use 'help' for usage information."); 
     } else {
-        match args[1].as_str() {
+        match args[1].to_ascii_lowercase().as_str() {
             "help" => {
                 display_help();
             },
@@ -39,14 +36,32 @@ fn create_bookmark(args: &Vec<String>) {
     if args.len() < 4 {
         println!("Please provide a name for the directory bookmark as well as a valid path.")
     } else {
-        let bookmark_name = &args[2];
         let bookmark_dir = Path::new(&args[3]);
         if bookmark_dir.is_dir() != true {
             println!("Please provide a valid directory path.")
         } else {
-            let mut bookmarks = check_for_file("create");
+            let bookmark_name = &args[2];
+            let mut bookmarks = fs::OpenOptions::new()
+                                    .read(true)
+                                    .append(true)
+                                    .create(true)
+                                    .open("cdd.txt");
+            match bookmarks {
+                Ok(mut file) => {
+                    modify_or_add(file, bookmark_name, bookmark_dir);
+                },
+                Err(e) => {
+                    println!("Encountered error opening file: {:?}", e);
+                },
+            }
         }
     }
+}
+
+fn modify_or_add(mut file: fs::File, bname: &str, bdir: &Path) {
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    println!("{:?}", contents);
 }
 
 fn remove_bookmark(args: &Vec<String>) {
@@ -55,28 +70,4 @@ fn remove_bookmark(args: &Vec<String>) {
 
 fn change_dir(args: &Vec<String>) {
 //                println!("Please provide a valid subcommand. Use 'help' for usage information.")
-}
-
-fn check_for_file(operation: &str) -> fs::File {
-    let mut bookmarks = fs::File::open("cdd.json");
-    match bookmarks {
-        Ok(f) => {
-            return f
-        },
-        Err(e) => {
-            match e {
-                NotFound => {
-                    if operation == "create" {
-                        let mut bookmarks = fs::File::create("cdd.json");
-                        return bookmarks.unwrap()
-                    } else {
-                        panic!("Bookmark file not found. Please run with the create command to create a new one.")
-                    }
-                },
-                _ => {
-                    panic!("Encountered error: {:?}", e);
-                },
-            }
-        },
-    }
 }
